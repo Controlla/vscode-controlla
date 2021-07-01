1cf83be76c681dc1c228d6d50a4f5730cfad39fe
Ivan Sotelo <isotelo@controlla.com.mx>
2021-06-30 18:20:31 -0600

    chore: Init project

---
+++ src/constants.ts
@@ -0,0 +1,16 @@
+// export const COMMAND_API_KEY = 'controlla.apikey';
+// export const COMMAND_DEVELOPER_ID = 'controlla.developerid';
+// export const COMMAND_PROXY = 'controlla.proxy';
+// export const COMMAND_DEBUG = 'controlla.debug';
+// export const COMMAND_STATUS_BAR_ENABLED = 'controlla.status_bar_enabled';
+// export const COMMAND_STATUS_BAR_CODING_ACTIVITY = 'controlla.status_bar_coding_activity';
+export const COMMAND_DASHBOARD = 'controlla.dashboard';
+export const COMMAND_NEW_FEATURE = 'controlla.new-feature';
+// export const COMMAND_CONFIG_FILE = 'controlla.config_file';
+// export const COMMAND_LOG_FILE = 'controlla.log_file';
+export enum LogLevel {
+    DEBUG = 0,
+    INFO,
+    WARN,
+    ERROR,
+}
